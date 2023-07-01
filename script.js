66d7c2987c1e8e35645b9c343072b947be77312e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-01 12:19:07 +0700

    fungsi ngambil input user udah berhasil

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const hasil = document.querySelector('.hasil');
+const display = document.querySelector('.hasil');
 const operator = document.querySelector('.operator');
 const userInput = document.querySelector('.user-input');
+let keys = '';
 
 // ngambil operator
 operator.addEventListener('click', function(e){
@@ -10,5 +11,11 @@ operator.addEventListener('click', function(e){
 
 // ngambil inputan user
 userInput.addEventListener('click', e => {
-  console.log(e.target.innerText)
-})
+  if(e.target.innerText === 'C'){
+    display.innerText = '0'
+    keys = '';
+  }else {
+    keys += e.target.innerText;
+    display.innerText = keys;
+  };
+});
