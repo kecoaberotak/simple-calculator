4fcbd733be2445888d9b836ea8b756805c31731e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-01 17:31:30 +0700

    fungsi tambah dan kurang udah jadi

--- script.js
+++ script.js
@@ -1,21 +1,101 @@
 const display = document.querySelector('.hasil');
 const operator = document.querySelector('.operator');
 const userInput = document.querySelector('.user-input');
-let keys = '';
+const clear = document.querySelector('.c');
+const samaDengan = document.querySelector('.sama-dengan');
 
-// ngambil operator
-operator.addEventListener('click', function(e){
-  console.log(e.target.className);
-})
+let value1 = '';
+let value2 = '';
+let condition = '';
+let hasil = 0;
 
-
-// ngambil inputan user
-userInput.addEventListener('click', e => {
-  if(e.target.innerText === 'C'){
-    display.innerText = '0'
-    keys = '';
-  }else {
-    keys += e.target.innerText;
-    display.innerText = keys;
-  };
+// ngambil input user
+userInput.addEventListener('click', function(e){
+  if(condition === ''){
+    if(e.target.textContent != 'C' && e.target.textContent != '='){
+      value1 += e.target.textContent;
+      display.textContent = value1;
+      console.log('value1', value1, typeof value1)
+    }
+  }else if(condition != ''){
+    if(e.target.textContent != 'C' && e.target.textContent != '='){
+      value2 += e.target.textContent;
+      display.textContent = value2;
+      console.log('value2', value2, typeof value2)
+    }
+  }
 });
+
+
+// ngambil operasi
+operator.addEventListener('click', function(e){
+  switch (e.target.className) {
+    case 'tambah':
+      condition = '+';
+      break;
+
+    case 'kurang':
+      condition = '-';
+      break;
+  
+    default:
+      break;
+  }
+});
+
+
+// Kalkulasi
+samaDengan.addEventListener('click', () => {
+  switch (condition) {
+    case '+':
+      tambah();
+      break;
+
+    case '-':
+      kurang();
+      break;
+  
+    default:
+      break;
+  }
+
+
+  value1 = '';
+  value2 = '';
+  condition = '';
+});
+
+
+// tombol clear
+clear.addEventListener('click', () => {
+  value1 = '';
+  value2 = '';
+  condition = '';
+  display.textContent = 0;
+  hasil = 0;
+});
+
+
+// Fungsi Operasi
+// Tambah
+function tambah(){
+  if(value1 != ''){
+    hasil += parseFloat(value1) + parseFloat(value2);
+  }else hasil += parseFloat(value2);
+  display.textContent = hasil;
+  return hasil;
+};
+
+// Kurang
+function kurang(){
+  if(hasil === 0){
+    hasil = value1;
+    hasil -= value2;
+  }else if(hasil != 0 && hasil >= 0){
+    hasil -= value2;
+  }else if(hasil < 0){
+    hasil += parseFloat(value2);
+  }
+  display.textContent = hasil;
+  return hasil;
+}
